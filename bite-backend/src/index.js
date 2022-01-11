fc9fa2e9ade41cb853665467a68b935cd9b7d193
Enrique <edavalos@ucsc.edu>
2022-01-10 16:08:10 -0800

    Formatting (Added semicolons)

--- bite-backend/src/index.js
+++ bite-backend/src/index.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const keysfile = require('./keys')
+const express = require('express');
+const keysfile = require('./keys');
+// access keys with keysfile.keys.clientID & keysfile.keys.APIKey
 
-const app = express()
-const port = 8000
+const app = express();
+const port = 8000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! ' + keysfile.keys.clientID)
-})
+  res.send('Hello World! ');
+});
 
 app.listen(port, () => {
-  console.log('Server listening at http://localhost:' + port)
-})
+  console.log('Server listening at http://localhost:' + port);
+});
