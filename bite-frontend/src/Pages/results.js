bcdce319db18aa629cc182536dcdc2b3b1d62843
Enrique <edavalos@ucsc.edu>
2022-01-18 14:54:23 -0800

    Added results template page

---
+++ bite-frontend/src/Pages/results.js
@@ -0,0 +1,21 @@
+import '../App.css';
+import React, { Component } from 'react';
+import LandingSearch from '../Components/LandingSearch/LandingSearch';
+import logo from '../logo_bite.png'
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <div className="LandingPage">
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div>
+          <LandingSearch />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
