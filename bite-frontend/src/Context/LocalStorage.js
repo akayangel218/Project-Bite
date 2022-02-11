94542b9ddad1133106ce2b7bbb7ce05d3a5b69be
Enrique <edavalos@ucsc.edu>
2022-02-11 03:44:42 -0800

    Completed functions for managing likes & dislikes

--- bite-frontend/src/Context/LocalStorage.js
+++ bite-frontend/src/Context/LocalStorage.js
@@ -1,25 +1,94 @@
 // This file holds all the functions that handle the user's liked and disliked restaurant lists
 
+// ===== Constants =====
+const likes_keyname = 'likes';
+const dislikes_keyname = 'dislikes';
+
 // ===== Functions to add & remove likes & dislikes (for modifying) =====
-function addRestaurantToLikes(restaurant) {};
+function addRestaurantToLikes(restaurant) {
+  const allLikes = getAllLikes();
+  allLikes.push(restaurant);
+  localStorage.setItem(likes_keyname, JSON.stringify(allLikes));
+};
 
-function removeRestaurantFromLikes(restaurantID) {};
+function removeRestaurantFromLikes(restaurantID) {
+  const newAllLikes = getAllLikes().filter(restaurant => restaurant.id != restaurantID);
+  localStorage.setItem(likes_keyname, JSON.stringify(newAllLikes));
+};
 
-function addRestaurantToDislikes(restaurant) {};
+function addRestaurantToDislikes(restaurant) {
+  const allDislikes = getAllDislikes();
+  allDislikes.push(restaurant);
+  localStorage.setItem(dislikes_keyname, JSON.stringify(allDislikes));
+};
 
-function removeRestaurantFromDislikes(restaurant) {};
+function removeRestaurantFromDislikes(restaurantID) {
+  const newAllDislikes = getAllDislikes().filter(restaurant => restaurant.id != restaurantID);
+  localStorage.setItem(dislikes_keyname, JSON.stringify(newAllDislikes));
+};
 
 // ===== Functions to get all likes & dislikes (for listing / displaying) =====
-function getAllLikes() {};
+function getAllLikes() {
+  const allLikesUnparsed = localStorage.getItem(likes_keyname);
+  if (allLikesUnparsed == null) return [];
+  else return JSON.parse(allLikesUnparsed);
+};
 
-function getAllDislikes() {};
+function getAllDislikes() {
+  const allDislikesUnparsed = localStorage.getItem(dislikes_keyname);
+  if (allDislikesUnparsed == null) return [];
+  else return JSON.parse(allDislikesUnparsed);
+};
 
 // ===== Functions to extract & format info from likes & dislikes (for sending to backend) =====
-function getTopCuisines() {};
+function getTopCuisines() {
+  const cuisines = {};
+  getAllLikes().forEach(restaurant => {
+    const cuisine = restaurant.cuisineCodes[0];
+    if (cuisine != null) {
+      if (cuisines[cuisine] == null) {
+        cuisines[cuisine] = 1;
+      } else {
+        cuisines[cuisine] += 1;
+      }
+    }
+  });
 
-function getTopPrices() {};
+  const cuisineList = [];
+  for (const cuisineCode in cuisines) {
+    cuisineList.push(cuisineCode + ':' + cuisines[cuisineCode]);
+  }
 
-function getDislikedIDs() {};
+  return cuisineList;
+};
+
+function getTopPrices() {
+  const prices = {};
+  getAllLikes().forEach(restaurant => {
+    const price = (restaurant.price).length;
+    if (prices[price] == null) {
+      prices[price] = 1;
+    } else {
+      prices[price] += 1;
+    }
+  });
+
+  const priceList = [];
+  for (const priceKey in prices) {
+    priceList.push(priceKey + ':' + prices[priceKey]);
+  }
+
+  return priceList;
+};
+
+function getDislikedIDs() {
+  const dislikedIDs = [];
+  getAllDislikes().forEach(restaurant => {
+    dislikedIDs.push(restaurant.id);
+  });
+  
+  return dislikedIDs;
+};
 
 // ===== Exports =====
 module.exports = {
